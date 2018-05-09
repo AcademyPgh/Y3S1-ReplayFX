24f57fa21dfa655a87f399a2fbe51689bc123e38
[Coral E] <[coral.412@outlook.com]>
2018-05-09 14:00:07 -0400

    add methods to apiscreens

--- screens/APIScreen.js
+++ screens/APIScreen.js
@@ -1,64 +1,85 @@
 import React, { Component } from 'react';
 import {
- Platform,
- StyleSheet,
- Text,
- Button,
- View,
- ScrollView,
+  Platform,
+  StyleSheet,
+  Text,
+  Button,
+  View,
+  ScrollView,
 } from 'react-native';
 import axios from 'axios';
 
 export default class APIScreen extends React.Component {
- constructor(props){
-   super(props);
-   
-   this.state = {
-     events:[]
-   };
- }
- componentDidMount()
- {
-   this.loadEvents();
- }
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      eventCategories: [],
+      replayEvents:[],
+      games: [],
+      gameTypes: [],
+    };
+  }
+  componentDidMount()
+  {
+    this.loadEventCategories();
+    this.loadEvents();
+    this.loadGames();
+    this.loadGameTypes();
+  }
+  loadEventCategories() {
+    axios
+      .get("http://replayfxcalendar.azurewebsites.net/public/categories")
+      .then((res) => {
+        let temp = res.data;
+        this.setState({ eventCategories: temp });
+      });
+  }
 
- loadEvents()
- {
-   axios
-   .get ("http://replayfxcalendar.azurewebsites.net/public")
-   .then((res) => {
-     let temp = res.data;
-     this.setState({ events: temp});
-   });
- }
- //http://replayfxcalendar.azurewebsites.net/public
-render() {
-     const { params } = this.props.navigation.state;
-     //const filter = params.scheduleFilter ? params.scheduleFilter : null;
-     const filter = this.props.filter;
-     
-     return (
+  loadEvents()
+  {
+    axios
+    .get ("http://replayfxcalendar.azurewebsites.net/public")
+    .then((res) => {
+      let temp = res.data;
+      this.setState({ replayEvents: temp});
+    });
+  }
+
+  loadGames()
+  {
+    axios
+      .get("http://replayfxcalendar.azurewebsites.net/public/games")
+      .then((res) => {
+        let temp = res.data;
+        this.setState({ games: temp});
+      });
+  }
+  loadGameTypes()
+  {
+    axios
+    .get("http://replayfxcalendar.azurewebsites.net/public/gametypes")
+    .then((res) => {
+      let temp = res.data;
+      this.setState({ gameTypes: temp});
+    });
+  }
+  //http://replayfxcalendar.azurewebsites.net/public
+    render() {
+      const { params } = this.props.navigation.state;
+      //const filter = params.scheduleFilter ? params.scheduleFilter : null;
+      const filter = this.props.filter;
+      
+      return (
         <View>
-            <Text>This is some text</Text>
-            <ScrollView>
-            <Text style={styles.text}>{JSON.stringify(this.state.events)}</Text>
-            </ScrollView>
-            {/* <View>
-            {this.state.events.map((event, index) => {
-                return (
-                <View>
-                    <Text>{event.title}</Text>
-                </View>
-                );
-                })
-            })
-            </View> */}
-        </View>
-    )
- };
-}
-
-const styles = StyleSheet.create({
+        <Text>This is some text</Text>
+        <ScrollView>
+        <Text style={styles.text}>{JSON.stringify(this.state.games)}</Text>
+        </ScrollView>
+         </View>
+     )
+    }}
+  const styles = StyleSheet.create({
     text: {
       fontSize: 24,
     },
