cc0aefe59040729045c9e719fef34ff09c269d01
John Lange <lange.john.m@gmail.com>
2018-07-12 14:11:49 -0400

    More promos added

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -24,6 +24,18 @@ export default class Promo extends Component {
                     style={this.props.styles.promoContainer}
                     source={require('../../Images/klask-promo.jpg')}
                 />
+            </TouchableOpacity>,
+            <TouchableOpacity onPress={() => this.props.displayEventById(359)}>
+                <ScalableImage width={this.props.width}
+                    style={this.props.styles.promoContainer}
+                    source={require('../../Images/cosplay-contest.jpg')}
+                />
+            </TouchableOpacity>,
+            <TouchableOpacity onPress={() => this.props.displayEventById(338)}>
+                <ScalableImage width={this.props.width}
+                    style={this.props.styles.promoContainer}
+                    source={require('../../Images/steel-samurai.jpg')}
+                />
             </TouchableOpacity>
         ]
         this.value = Math.floor(Math.random() * this.promos.length);
