85b9d1dd80e8935fcf7c7d06ca5f1d3080b6e00f
Jim South <james.a.south@gmail.com>
2019-01-01 20:37:04 -0500

    Enabled sticky section headers on android

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -78,6 +78,7 @@ export default class ScheduleScreen extends Component {
           }
           ItemSeparatorComponent={this.renderSeparator}
           SectionSeparatorComponent={this.renderSeparator}
+          stickySectionHeadersEnabled
         />
       </View>
     );
