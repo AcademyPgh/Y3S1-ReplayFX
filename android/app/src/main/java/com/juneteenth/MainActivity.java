9c98d5238648505044a664a5afa315b85b227869
John Lange <lange.john.m@gmail.com>
2021-04-20 21:08:55 -0400

    Update android builds and keys

--- android/app/src/main/java/com/juneteenth/MainActivity.java
+++ android/app/src/main/java/com/juneteenth/MainActivity.java
@@ -1,4 +1,4 @@
-package com.academypgh.freshfest;
+package com.juneteenth;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "FreshFest";
+    return "Juneteenth";
   }
 }
