6a0c90bac286235adc414b96486d677e70181302
John Lange <lange.john.m@gmail.com>
2021-04-29 15:02:08 -0400

    Fix android rename issue

--- android/app/src/main/java/com/juneteenth/MainActivity.java
+++ android/app/src/main/java/com/juneteenth/MainActivity.java
@@ -1,4 +1,4 @@
-package com.juneteenth;
+package com.academypgh.juneteenth;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/juneteenth/MainApplication.java
+++ android/app/src/main/java/com/juneteenth/MainApplication.java
@@ -1,4 +1,4 @@
-package com.juneteenth;
+package com.academypgh.juneteenth;
 
 import android.app.Application;
 import android.content.Context;
@@ -69,7 +69,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.juneteenth.flipper.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.academypgh.juneteenth.flipper.ReactNativeFlipper");
         aClass.getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
       } catch (ClassNotFoundException e) {
