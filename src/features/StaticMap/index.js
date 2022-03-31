1f343be1290c1a17b38654da9d524d914d94a211
Sean Lenhart <theseanlenhart@gmail.com>
2022-03-31 15:38:32 -0400

    add PhotoView component for better android resolution

--- src/features/StaticMap/index.js
+++ src/features/StaticMap/index.js
@@ -4,6 +4,8 @@ import { homeButtonHeader } from '../../utils/Headers';
 import ImageZoom from 'react-native-image-pan-zoom';
 import ScalableImage from 'react-native-scalable-image';
 
+import PhotoView from 'react-native-photo-view-ex';
+
 export default class StaticMap extends Component {
   static navigationOptions = ({ navigation, navigationOptions }) => {
     const { params } = navigation.state;
@@ -18,14 +20,19 @@ export default class StaticMap extends Component {
   render() {
     return (  
       <View style={{flex: 1}}>
-        <ImageZoom cropWidth={Dimensions.get("window").width}
+        <PhotoView
+          style={{ flex: 1, width: '100%', height: '100%' }}
+          source={{uri: this.props.screenProps.apiData.mapImageUrl}}
+          minimumZoomScale={1}
+          maximumZoomScale={9} />
+        {/* <ImageZoom cropWidth={Dimensions.get("window").width}
               cropHeight={Dimensions.get("window").height}
               imageWidth={Dimensions.get("window").width}
               imageHeight={Dimensions.get("window").height}>
           <ScalableImage width={Dimensions.get("window").width} 
             source={{uri: this.props.screenProps.apiData.mapImageUrl}}
           />
-        </ImageZoom>
+        </ImageZoom> */}
       </View>
     );
   }
